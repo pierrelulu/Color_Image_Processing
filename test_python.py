10ff8dccd2d283e9ef34d713b20a298a37e72292
pierrelulu <luttmannpierre@gmail.com>
2025-03-18 10:18:30 +0100

    First draft

--- test_python.py
+++ test_python.py
@@ -1,1 +1,19 @@
-#test python
+
+# Lecture Data batch (B)
+
+# lecture data éclairage (L)
+
+# Calcul de X(w,j), Y(w,j), Z(w,j) pour chaque éclairage (indépendant de l'objet)
+
+# Calcul pour chaque pair (B,L)
+    # du X(i,j), Y(i,j), Z(i,j)
+
+    # du L*(i,j) a*(i,j) b*(i,j)
+
+# Recherche d'une paire d'éclairage tel que deux batch couleurs aient une distante Lab petite (tend vers 0) sur un éclairage et grande sur un autre
+
+# Extraction des RGB des deux batch et de l'indice des deux éclairages
+
+# génération d'une image au ratio feuille A5 avec le texte "PINTO_LUTTMANN" au centre de la couleur d'un batch, et le fond à la couleru de l'autre batch
+
+# enregistrer l'image au format png
